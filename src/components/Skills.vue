fe02d1c46c161603470f29907df9d74030670664
timothy okooboh <okoobohtimothy@gmail.com>
2020-06-11 00:05:28 -0700

    about to merge files

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,25 +1,65 @@
 <template>
   <div>
     <div class="mb-5">
-      <div class="title indigo--text">Front-end languages</div>
-      <div>HTML, CSS, Javascript</div>
-    </div>
-    <div class="mb-5">
-      <div class="title indigo--text">Front-end framework</div>
-      <div> Vue.js (Vue router, Vuex, Vuetify, Vuelidate), Bootstrap, Chart.js.
+      <div class="title my-3">Front-end languages</div>
+      <div class="flex-logos">
+        <div><img src="html_logo.png" alt="html logo"></div>
+        <div><img src="css_logo.png" alt="css logo"></div>
+        <div><img src="js_logo.png" alt="javascript logo"></div>
+        <div><img src="typescript_logo.png" alt="typescript logo"></div>
       </div>
     </div>
+
     <div class="mb-5">
-      <div class="title indigo--text">Server-side framework</div>
-      <div> Laravel</div>
+      <div class="title">Front-end framework</div>
+      <div class="flex-logos">
+        <div><img src="vuejs_logo.jpeg" alt="vue.js logo"></div>
+        <div><img src="nuxtjs_logo.jpg" alt="nuxt.js logo"></div>
+        <div><img src="chartjs_logo.png" alt="chart.js logo"></div>
+        <div><img src="bootstrap_logo.png" alt="bootstrap logo"></div>
+        <div><img src="sass_logo.png" alt="sass logo"></div>
+      </div>
     </div>
+
     <div class="mb-5">
-      <div class="title indigo--text">Server-side database</div>
-      <div> MySQL</div>
+      <div class="title">Server-side techonologies </div>
+      <div class="flex-logos">
+        <div><img src="nodejs_logo.png" alt="node.js logo"></div>
+        <div><img src="expressjs_logo.png" alt="express.js logo"></div>
+        <div><img src="laravel_logo.png" alt="laravel logo"></div>
+      </div>
     </div>
+
     <div class="mb-5">
-      <div class="title indigo--text">Version control</div>
-      <div> GIT/Github</div>
+      <div class="title">Server-side database</div>
+      <div class="flex-logos">
+        <div><img src="mongo_logo.png" alt="mongodb logo"></div>
+        <div><img src="mysql_logo.png" alt="mysql logo"></div>
+      </div>
+    </div>
+
+    <div class="mb-5">
+      <div class="title">Version control</div>
+      <div class="flex-logos">
+        <div><img src="git_logo.png" alt="git logo"></div>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .flex-logos {
+    display: flex;
+    flex-wrap: wrap;
+    justify-items: center;
+    justify-content: center;
+  }
+  .flex-logos div {
+    margin: 10px;
+  }
+  .flex-logos img {
+    width: 120px;
+    height: 100px;
+    object-fit: contain;
+  }
+</style>
